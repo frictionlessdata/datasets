0dde23bd1d858c6376ce6507a1f764f8265182c3
Dmytro Gierman <mryakzp@gmail.com>
2018-01-31 11:23:53 +0200

    [helper][s] data creates via 'lazy python generator' and there is helper messages.

--- csv-generator.py
+++ csv-generator.py
@@ -4,17 +4,27 @@ import csv
 def save_dict_to_csv(data, path):
     with open(path, 'w') as file:
         writer = csv.writer(file)
-        writer.writerows(data)
+        print('Creating "%s" file...' % path)
+        n = 1
+        for line in data:
+            writer.writerow(line)
+
+            n += 1
+            if n % 1000 == 0:
+                print(n, 'lines')
+        print('DONE!')
 
 
-def create_sample_table(rows=10, columns=10):
-    headers = ['header ' + str(n) for n in range(columns)]
-    values = [['row %d col %d' % (row, n) for n in range(columns)] for row in range(1, rows)]
-    return [headers] + values
+def sample_table_generator(rows=10, columns=10):
+    # headers
+    yield ['header %d' % n for n in range(columns)]
+    # values
+    for row in range(1, rows):
+        yield ['row %d col %d' % (row, n) for n in range(columns)]
 
 
 if __name__ == "__main__":
     save_dict_to_csv(
-        data=create_sample_table(rows=10, columns=1030),
-        path='data-files/csv/1030_columns.csv'
+        data=sample_table_generator(rows=10000, columns=1000),
+        path='data-files/csv/big.csv'
     )
